c698a6702970e1941345626e50d77b1837f7fc80
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-01-24 16:58:22 +0900

    Queue 12789 add

---
+++ baekjoon/Queue/12789.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+saveStack = deque([])
+
+input = int(input())
+inputs = deque(list(map(int, sys.stdin.readline().split())))
+
+fail = False
+cnt = 1
+while (len(inputs) > 0) or (len(saveStack) != 0):
+  num = -1
+  if (len(saveStack) != 0 and cnt == saveStack[-1]):
+    saveStack.pop()
+    cnt += 1
+  else:
+    if (len(inputs) != 0):
+      num = inputs.popleft()
+    if (cnt == num):
+      cnt += 1
+    else:
+      if (num != -1):
+        saveStack.append(num)
+      else:
+        fail = True
+        break
+  print('inputs', inputs)
+  print('saveStack', saveStack)
+  print('cnt', cnt)
+
+if (fail == True):
+  print('Sad')
+else:
+  print('Nice')
--- baekjoon/Queue/README.md
+++ baekjoon/Queue/README.md
@@ -6,3 +6,4 @@
 | [카드2 - 2164](https://www.acmicpc.net/problem/2164) | [문제풀이](./2164.py) |
 | [큐 - 10845](https://www.acmicpc.net/problem/10845) | [문제풀이](./10845.py) |
 | [큐2 - 18258](https://www.acmicpc.net/problem/18258) | [문제풀이](./18258.py) |
+| [도키도키 간식드리미 - 12789](https://www.acmicpc.net/problem/12789) | [문제풀이](./12789.py) |
