638f7e8da1d50cdf0a5f3079ba82387fa7bade72
zoz0312 <zoz0312@naver.com>
2021-10-29 04:14:37 +0900

    vuex store multi module add

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <h1>{{ message }}</h1>
+  <div>
+    <span>
+      counter: {{ counter }}
+    </span>
+    <span>
+      test: {{ test }}
+    </span>
+    <span>
+      doubleGetters: {{ doubleGetters }}
+    </span>
+  </div>
 </template>
 
 <script>
+import { computed, onMounted, toRef } from "vue";
+import { useStore } from 'vuex';
+
 export default {
+  setup() {
+    const store = useStore();
+    const counter = computed(() => store.state.seat.count);
+    const test = computed(() => store.getters);
+
+    const doubleGetters = computed(() => store.getters.doubleCount);
+    const inc = () => store.commit("setCounter", counter.value + 1);
+
+    return { counter, test, doubleGetters };
+  },
   data() {
     return {
       message : 'Hello Vue!!!'
--- vuejs/src/index.js
+++ vuejs/src/index.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import Vuex from 'vuex';
 import App from './App.vue';
 import { store } from './store/tk.main';
+import ReservationWaiting from './store/tk.reservation.waiting/index';
 
 const app = createApp(App);
 app.use(Vuex);
-app.use(store);
+// app.use(store);
+app.use(ReservationWaiting);
 app.mount('#app');
 
 window.tk = {};
