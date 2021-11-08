3ad13c7a68ba373eb841511dce7e24468d1bd28d
aju312 <aju312@ticketlink.co.kr>
2021-11-08 17:38:55 +0900

    change json to ts

--- react-mpa/src/i18n/i18n.ts
+++ react-mpa/src/i18n/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import tranKo from './ko.json'
+import tranKo from './ko'
 // import tranEn from './Files/en.json';
 
 const resources = {
